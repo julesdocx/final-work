077ff442ad6279ce83a0568da4e8edbc25a528d0
julesdocx <jules.docx@student.ehb.be>
2021-08-12 14:37:24 +0200

    styling(upload.component): user interfase

--- final-work-angular/src/app/app-components/upload/upload.component.ts
+++ final-work-angular/src/app/app-components/upload/upload.component.ts
@@ -1,37 +1,100 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {ChangeDetectorRef, Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { QuillConfiguration } from "./quill-configuration";
+import Quill from 'quill';
 
+const font = Quill.import('formats/font')
+
+font.whitelist = ['PT serif', 'Neue Haas Grotesk', 'PT sans']
+Quill.register(font, true);
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40
+}
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+  styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent implements OnInit {
-  textEditorForm: FormGroup;
+  storyForm: FormGroup;
+  selectedChapter: number = 0;
+  quillConfiguration = QuillConfiguration;
   preview: boolean = false;
+  editMetaData: boolean = false;
+  chapterCount: number = 0;
 
   constructor(private formBuilder: FormBuilder) {
-    this.textEditorForm = this.formBuilder.group({
-      title: [
-        'Chapter Title 1'
-      ],
-      text: [
-        `<h2><u>This is </u>a <span style=\"color: rgb(240, 102, 102);\">RICH</span> <strong>text editor</strong> <em>for</em> </h2><h3><span style=\"color: rgb(153, 51, 255);\">I hope you </span><strong style=\"color: rgb(153, 51, 255);\">like it!</strong></h3>`
-      ]
+    this.storyForm = this.formBuilder.group({
+      title: ['Untitled', Validators.required],
+      description: [''],
+      author: [''],
+      chapters: this.formBuilder.array([]),
     });
+    this.addChapter();
+    console.log('constructor')
   }
 
-  ngOnInit() {
-  }
-
-
-  get textControls() {
-    return this.textEditorForm.controls.text as FormControl;
-  }
+  ngOnInit() {}
 
   stringify(value: string) {
     var parser = new DOMParser();
-    var element = parser.parseFromString(value, "text/html")
+    var element = parser.parseFromString(value, "text/html");
     return element;
+  }
+
+  addChapter() {
+    this.chapterCount += 1;
+    this.chapters.push(this.newChapter());
+  }
+
+  get chapters(): FormArray {
+    return this.storyForm.get("chapters") as FormArray;
+  }
+
+  get chapterControls() {
+    return this.chapters['controls'];
+  }
+
+  newChapter(): FormGroup {
+    return this.formBuilder.group({
+      name: [`Chapter ${this.chapters.length + 1}`],
+      text: [''],
+    })
+  }
+
+  removeChapter(i: number) {
+    this.chapters.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.storyForm.controls);
+  }
+
+  checkOverflow (element: any) {
+
+    if (element.offsetHeight < element.scrollHeight ||
+        element.offsetWidth < element.scrollWidth) {
+          console.log(true);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(this.selectedChapter, this.chapterCount);
+
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW || event.keyCode === KEY_CODE.UP_ARROW) {
+      this.selectedChapter == this.chapterCount? this.selectedChapter += 1: null
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW || event.keyCode === KEY_CODE.DOWN_ARROW) {
+      this.selectedChapter == 0? this.selectedChapter -= 1: null
+    }
   }
 }
